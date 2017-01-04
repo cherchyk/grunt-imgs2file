05a33b5ef219c8bbee21c83f6c3d3290fa434838
Bohdan Cherchyk <bcherchyk@salesforce.com>
2017-01-03 21:46:58 -0500

    ts

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,64 +10,107 @@
 
 module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			all: ['tmp', 'tasks/**/*'],
+			tests: ['tmp'],
+			release: ['tasks/.baseDir*']
+		},
 
-    // Configuration to be run (and then tested).
-    imgs2file: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
+		// Configuration to be run (and then tested).
+		imgs2file: {
+			default_options: {
+				options: {
+				},
+				files: {
+					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			},
+			custom_options: {
+				options: {
+					separator: ': ',
+					punctuation: ' !!!'
+				},
+				files: {
+					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		},
 
-  });
+		ts: {
+			app: {
+				files: [{
+					src: ['src/**/*.ts', '!src/.baseDir.ts'],
+					dest: 'tasks'
+				}],
+				options: {
+					module: "commonjs",
+					noLib: false,
+					target: "es6",
+					sourceMap: true
+				}
+			}
+		},
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+		tslint: {
+			src: {
+				options: {
+					configuration: "tslint.json"
+				},
+				files: {
+					src: [
+						"src/**/*.ts"
+					]
+				}
+			},
+			test: {
+				options: {
+					configuration: "tslint.json"
+				},
+				files: {
+					src: [
+						"test-src/**/*.ts",
+						"!test-src/routes/**/*.ts"
+					]
+				}
+			}
+		},
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	});
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'imgs2file', 'nodeunit']);
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-ts');
+	grunt.loadNpmTasks("grunt-tslint");
+
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean:tests', 'imgs2file', 'nodeunit']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['clean:all', 'ts', 'jshint', 'clean:release', 'test']);
 
 };
---
+++ src/imgs2file.ts
@@ -0,0 +1,57 @@
+/*
+ * grunt-imgs2file
+ * https://github.com/cherchyk/grunt-imgs2file
+ *
+ * Copyright (c) 2017 Bohdan Cherchyk
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+export = function (grunt) {
+
+	// Please see the Grunt documentation for more information regarding task
+	// creation: http://gruntjs.com/creating-tasks
+
+	grunt.registerMultiTask('imgs2file', 'Collects image files in target folder and writes their content to destination file in Base64.', function () {
+
+		grunt.log.warn('HELLO.');
+
+
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			punctuation: '.',
+			separator: ', '
+		});
+
+		// Iterate over all specified file groups.
+		this.files.forEach(function (f) {
+			// Concat specified files.
+			var src = f.src.filter(function (filepath) {
+				// Warn on and remove invalid source files (if nonull was set).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				}
+				else {
+					return true;
+				}
+			})
+				.map(function (filepath) {
+					// Read file source.
+					return grunt.file.read(filepath);
+				})
+				.join(grunt.util.normalizelf(options.separator));
+
+			// Handle options.
+			src += options.punctuation;
+
+			// Write the destination file.
+			grunt.file.write(f.dest, src);
+
+			// Print a success message.
+			grunt.log.writeln('File "' + f.dest + '" created.');
+		});
+	});
+
+}
